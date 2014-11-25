db0dcabace0a6ad6fe602fab851a7f8ffbad5154
gtupak <gabriel.tapuc@gmail.com>
2014-11-24 19:22:48 -0500

    Fixed test case.

--- src/ServerTest.java
+++ src/ServerTest.java
@@ -9,10 +9,10 @@ import common.Entry;
 
 public class ServerTest {
 
-	private static EchoServer server;
+	private static  EchoServer server;
 	private static ChatClient client;
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws InterruptedException{
 		server = new EchoServer(server.DEFAULT_PORT, new ChatIF(){
 
 			@Override
@@ -69,24 +69,31 @@ public class ServerTest {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+		System.out.println("---TEST 1: Adding entires---");
 		client.handleMessageFromClientUI("#login " + client.getLogin());
 		client.handleMessageFromClientUI("#sql INSERT INTO entries VALUES(0, 'Item list 0', '"+ client.getLogin()+"', null, false)");
 		client.handleMessageFromClientUI("#sql INSERT INTO entries VALUES(1, 'Item list 1', '"+ client.getLogin()+"', null, false)");
 		client.handleMessageFromClientUI("#sql INSERT INTO entries VALUES(2, 'Item list 2', '"+ client.getLogin()+"', null, false)");
-
+		Thread.sleep(2000);
 
 		System.out.println();
+		System.out.println("---TEST 2: Deleting entry with ID 0---");
 		client.handleMessageFromClientUI("#sql DELETE FROM entries WHERE id=0");
-
+		Thread.sleep(2000);
+		
 		System.out.println();
+		System.out.println("---TEST 3: Editing entry with ID 1---");
 		client.handleMessageFromClientUI("#sql UPDATE entries SET entry='Item list 1 --#-- EDITED' WHERE id=1");
-
+		Thread.sleep(2000);
+			
 		System.out.println();
+		System.out.println("---TEST 4: Checking entry with ID 2---");
 		client.handleMessageFromClientUI("#sql UPDATE entries SET checked=TRUE, checker='" 
 				+ client.getLogin() + "' WHERE id=2");
+		Thread.sleep(2000);
 
 		System.out.println();
+		System.out.println("---TEST 5: Unchecking entry with ID 2---");
 		client.handleMessageFromClientUI("#sql UPDATE entries SET checked=FALSE, checker=null WHERE id=2");
 
 	}
